72da2a4179ee691059d37d77e21c05cd141b3e45
echo <play3step@naver.com>
2024-03-08 23:07:06 +0900

    fix: build 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Main from './pages/Main.jsx'
+import Main from './pages/Main'
 
 function App() {
   return (
