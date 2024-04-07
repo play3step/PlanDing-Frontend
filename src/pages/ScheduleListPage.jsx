36812b31f3f7dffb7a63563149683dd1b0632efa
echo <play3step@naver.com>
2024-04-07 23:30:28 +0900

    feat: 그룹 리스트 세팅 api 추가

--- src/pages/ScheduleListPage.jsx
+++ src/pages/ScheduleListPage.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react'
 import ScheduleAdd from '../components/ScheduleListPage/ScheduleAdd'
+import { useDispatch, useSelector } from 'react-redux'
+import { setScheduleList } from '../api/Schedule/ScheduleList'
 
 const ScheduleListPage = () => {
+  const dispatch = useDispatch()
+  const userInfo = useSelector(state => state.users.user)
+  useEffect(() => {
+    if (userInfo) {
+      dispatch(setScheduleList(userInfo.token))
+    }
+  })
   return <ScheduleAdd />
 }
 export default ScheduleListPage
